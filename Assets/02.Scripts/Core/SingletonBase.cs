d65c196dfe52ea67d9cb7ccf3b17009dfb451ea1
신영준 <kdrsa789@gmail.com>
2026-08-10 23:54:57 +0900

    [신영준][UI] feat: UI 매니저

---
+++ Assets/02.Scripts/Core/SingletonBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _instance = this as T;
+    }
+}
