f5c863e41bda39897a9e030949bbd16b3780a1b5
이주헌 <Volume6214@gmail.com>
2026-08-11 17:49:11 +0900

    [이주헌][Map] refactor: MapManager 수정
    - UI 연동을 위해서 수정

--- Assets/02.Scripts/Map/MapManager.cs
+++ Assets/02.Scripts/Map/MapManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using Random = UnityEngine.Random;
 
 public class MapManager : SingletonBase<MapManager>
 {
@@ -21,6 +23,9 @@ public class MapManager : SingletonBase<MapManager>
     };
 
     private Dictionary<Vector3Int, GameObject> _spawnedMaps = new Dictionary<Vector3Int, GameObject>();
+    private Dictionary<Vector3Int, int> _mapTypeData = new Dictionary<Vector3Int, int>();
+
+    public event Action<Dictionary<Vector3Int, int>> OnMapGenerated;
 
     private void Awake()
     {
@@ -59,6 +64,7 @@ public class MapManager : SingletonBase<MapManager>
 
         Assert.IsNotNull(_centralTerminalPrefab, "[MapManager] Central Terminal Prefab이 할당되지 않았습니다!");
         SpawnMapObject(_centralTerminalPrefab, Vector3Int.zero, "CentralTerminal");
+        _mapTypeData[Vector3Int.zero] = 2;
 
         List<bool> assignedTypes = RandomStationLayout();
 
@@ -67,6 +73,8 @@ public class MapManager : SingletonBase<MapManager>
             Vector3Int gridPos = _mapOffsets[i];
             bool isStation = assignedTypes[i];
             GameObject selectedPrefab = null;
+
+            int typeId = isStation ? 1 : 0;
 
             if (isStation)
             {
@@ -97,6 +105,8 @@ public class MapManager : SingletonBase<MapManager>
         }
 
         Debug.Log("[MapManager] 3x3 맵 생성 및 규칙 배치 완료!");
+
+        OnMapGenerated?.Invoke(_mapTypeData);
     }
 
     private List<bool> RandomStationLayout()
@@ -173,5 +183,10 @@ public class MapManager : SingletonBase<MapManager>
             }
         }
         _spawnedMaps.Clear();
+    }
+
+    public Dictionary<Vector3Int, int> GetMapTypeData()
+    {
+        return _mapTypeData;
     }
 }
