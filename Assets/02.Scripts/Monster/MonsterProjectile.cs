f7e39ec8f012db26bc96b5c20e27751220b20743
나영준 <blueair0379@gmail.com>
2026-08-20 13:53:35 +0900

    [나영준][Monster] feat: 투사체에 디버프 공격타입 추가

--- Assets/02.Scripts/Monster/MonsterProjectile.cs
+++ Assets/02.Scripts/Monster/MonsterProjectile.cs
@@ -2,33 +2,41 @@
 
 public class MonsterProjectile : MonoBehaviour
 {
-    private float speed = 15f;
-    private int damage;
-    private Vector3 direction;
+    private float _speed = 15f;
+    private int _damage;
+    private Vector3 _direction;
 
-    private float currentTime = 0f;
-    private float lifeTime = 2f;
+    private string _attackType;
+    private float _debuffDuration;
+    private float _debuffPower;
 
-    public void ProjectileInitialize(Vector3 dir, int atk)
+    private float _currentTime = 0f;
+    private float _lifeTime = 2f;
+
+    public void ProjectileInitialize(Vector3 dir, int atk, string type = "None", float duration = 0f, float power = 0f)
     {
-        direction = dir.normalized;
-        damage = atk;
+        _direction = dir.normalized;
+        _damage = atk;
 
-        currentTime = 0f;
+        _attackType = type;
+        _debuffDuration = duration;
+        _debuffPower = power;
 
-        if (direction != Vector3.zero)
+        _currentTime = 0f;
+
+        if (_direction != Vector3.zero)
         {
-            transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0f, 90f, 0f);
+            transform.rotation = Quaternion.LookRotation(_direction) * Quaternion.Euler(0f, 90f, 0f);
         }
 
     }
 
     private void Update()
     {
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position += _direction * _speed * Time.deltaTime;
 
-        currentTime += Time.deltaTime;
-        if (currentTime >= lifeTime)
+        _currentTime += Time.deltaTime;
+        if (_currentTime >= _lifeTime)
         {
             PoolManager.Instance.DespawnToPool(gameObject);
         }
@@ -38,12 +46,13 @@ public class MonsterProjectile : MonoBehaviour
     {
         if (other.CompareTag("Train") || other.GetComponent<Train>() != null)
         {
-            Debug.Log("Hit Player");
+            Debug.Log("플레이어 타격");
 
             // TrainHealth trainHp = other.GetComponent<TrainHealth>();
             // if (trainHp != null)
             // {
-            //   trainHp.TakeDamage(damage);
+            //   trainHp.TakeDamage(_damage);
+            //   trainHp.ApplyDebuff(_attackType, _debuffDuration, _debuffPower);
             // }
 
             PoolManager.Instance.DespawnToPool(gameObject);
