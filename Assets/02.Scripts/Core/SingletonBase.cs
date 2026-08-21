ac2cc2132a6df0bf972fa3c250545cc2949c13bd
이태영 <scammerlee1943@gmail.com>
2026-08-21 16:11:12 +0900

    [이태영][Core] refactor: SingletonBase 중복 시 컴포넌트만 제거

--- Assets/02.Scripts/Core/SingletonBase.cs
+++ Assets/02.Scripts/Core/SingletonBase.cs
@@ -5,16 +5,17 @@ public class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour
     private static T _instance;
     public static T Instance => _instance;
 
-    private void Awake()
+    protected virtual void Awake()
     {
         Init();
     }
 
     protected virtual void Init()
     {
-        if (_instance != null)
+        if (_instance != null && _instance != this)
         {
-            Destroy(gameObject);
+            Debug.LogWarning($"[{typeof(T).Name}] 이미 인스턴스가 있어 이 컴포넌트를 제거합니다.");
+            Destroy(this);
             return;
         }
         _instance = this as T;
