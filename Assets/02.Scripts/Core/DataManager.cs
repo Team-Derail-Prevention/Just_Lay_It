7f103cf9a2fc0dc15fefbbe87c628a9e00f5ba5c
강혜창 <a4076a4076@gmail.com>
2026-08-24 15:11:18 +0900

    [강혜창][Weapon] feat: 무기 데이터 추가
    무기 Id
    무기 이름
    공격력
    발사속도
    탄창수
    재장전 시간
    발사 소리
    장전 소리

--- Assets/02.Scripts/Core/DataManager.cs
+++ Assets/02.Scripts/Core/DataManager.cs
@@ -71,6 +71,7 @@ public class DataManager : SingletonBase<DataManager>
         await LoadDataAsync<MonsterData>("MonsterTableData", cancellationToken);
         await LoadDataAsync<MaterialObjectData>("MaterialObjectData", cancellationToken);
         await LoadDataAsync<TrainData>("TrainData", cancellationToken);
+        await LoadDataAsync<WeaponData>("WeponData", cancellationToken);
 
         IsLoaded = true;
         OnDataLoadCompleted?.Invoke();
