85ab9c7ad1cd6f120fb1763445afab63e7c8658a
신영준 <kdrsa789@gmail.com>
2026-08-24 13:42:18 +0900

    [신영준][UI] fix : 인 게임에서 로비로 나가기 안되는거 수정

--- Assets/02.Scripts/Core/GameManager.cs
+++ Assets/02.Scripts/Core/GameManager.cs
@@ -458,6 +458,12 @@ public class GameManager : SingletonBase<GameManager>
         ChangeGameState(GameState.Ready);
         NetworkResourceService.ResetRun();
         NetworkWarehouseService.ResetRun();
+
+        UI?.CloseHudTrainStatusUI();
+        UI?.CloseHudResourceUI();
+        UI?.CloseInGameMenuButtonUI();
+        UI?.CloseRailBuildUI();
+
         UI?.OpenContentUI(UIType.LobbyUI);
     }
 
