27f21e32017b2dafe0653d1e00ea8397d5cdd668
이주헌 <Volume6214@gmail.com>
2026-08-25 17:21:12 +0900

    [이주헌][GameManager] feat: 게임클리어 조건 추가

--- Assets/02.Scripts/Core/GameState.cs
+++ Assets/02.Scripts/Core/GameState.cs
@@ -4,5 +4,6 @@
     Playing = 1,          // 필드 전투 및 생존 탐색
     EventPaused = 2,      // 역 도달 및 이벤트 (시간 정지)
     ExitSelected = 3,     // 출구 방향 선택 완료 후 재개 준비
-    GameOver = 4          // 게임 오버
+    GameOver = 4,         // 게임 오버
+    GameClear = 5,        // 게임 클리어  
 }
