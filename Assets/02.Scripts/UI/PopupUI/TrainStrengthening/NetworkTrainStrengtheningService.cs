fa9c6f33443b027de0ff1363d135bd5d5ef403ac
신영준 <kdrsa789@gmail.com>
2026-08-28 13:23:10 +0900

    [신영준][UI] fix: 무기 장착 인벤토리 방식 수정

--- Assets/02.Scripts/UI/PopupUI/TrainStrengthening/NetworkTrainStrengtheningService.cs
+++ Assets/02.Scripts/UI/PopupUI/TrainStrengthening/NetworkTrainStrengtheningService.cs
@@ -2,9 +2,6 @@
 
 public class NetworkTrainStrengtheningService : SingletonBase<NetworkTrainStrengtheningService>
 {
-    // 적제 업글에 따른 해금 칸 정해지면 추후 수정
-    private const int CARGO_BASE_UNLOCKED_COUNT = 3;
-
     private TrainStrengtheningViewModel _localVm;
 
     private void Start()
@@ -82,8 +79,7 @@ public class NetworkTrainStrengtheningService : SingletonBase<NetworkTrainStreng
     {
         if (slotDataId == "CARGO_WEAPON_LIMIT")
         {
-            int unlockedCount = CARGO_BASE_UNLOCKED_COUNT + newLevel;
-            NetworkAugmentService.Instance.SetEquipUnlockedCount(unlockedCount);
+            NetworkAugmentService.Instance.ApplyWeaponSlotUnlockLevel(newLevel);
         }
 
         UpgradeEventHub.Instance.NotifyInGameUpgraded(slotDataId, newLevel);
