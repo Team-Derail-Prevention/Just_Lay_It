9c527e776a6f48e7a733568c8fc9fc100ef2d68c
이주헌 <Volume6214@gmail.com>
2026-08-31 13:13:45 +0900

    [이주헌][Camera] feat: FollowCamera에 시점 추가

--- Assets/02.Scripts/Core/GameManager.cs
+++ Assets/02.Scripts/Core/GameManager.cs
@@ -620,11 +620,6 @@ public class GameManager : SingletonBase<GameManager>
         // TODO: Terminal UI를 열고 CentralTerminal.SelectExitGate(int)와 연결필요(?)
     }
 
-    private void OpenBaseArrivalAfterScore()
-    {
-        UI?.OpenBaseArrivalUI();
-    }
-
     private void RemoveCompletedStations()
     {
         foreach (StationObject station in _completedStations)
@@ -828,7 +823,10 @@ public class GameManager : SingletonBase<GameManager>
 
     private bool ValidateManager(Component manager, string managerName)
     {
-        if (manager != null) return true;
+        if (manager != null)
+        {
+            return true;
+        }
 
         Debug.LogError($"[GameManager] 필수 매니저 '{managerName}'를 찾지 못했습니다.");
         return false;
