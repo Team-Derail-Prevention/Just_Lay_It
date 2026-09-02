5406566d5253e6a340c0607800368eec7cc67586
강혜창 <a4076a4076@gmail.com>
2026-09-02 14:38:54 +0900

    [강혜창][Rail] fix: D인풋 키 UI로 이전

--- Assets/02.Scripts/Rail/RailManager.cs
+++ Assets/02.Scripts/Rail/RailManager.cs
@@ -155,14 +155,6 @@ public class RailManager : SingletonBase<RailManager>
             RemoveAllRail();
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            if (_isPlaceModeActive && _isHoveredCube)
-            {
-                TryInstallRail(_hoveredGridIndex, _hoveredCubeInfo);
-            }
-        }
-
         if (!_isPlaceModeActive)
         {
             return;
