553aeff754b8012958c11734a9add56d080136bf
정명진 <audwls3159@gmail.com>
2026-09-21 11:10:26 +0900

    [정명진][Monster] feat: 몬스터 사망 이펙트 프리팹  stopAction 값 수정, 기존 몬스터 프리팹 사망이펙트 코드 수정

--- Assets/02.Scripts/Monster/MonsterDeathEffect.cs
+++ Assets/02.Scripts/Monster/MonsterDeathEffect.cs
@@ -1,59 +1,67 @@
 ﻿using Cysharp.Threading.Tasks;
+using System.Threading;
 using UnityEngine;
 
 public class MonsterDeathEffect : MonoBehaviour
 {
-    [SerializeField] private ParticleSystem _particle;
-
     private ParticleSystem[] _particles;
+    private CancellationTokenSource _despawnCts;
 
     private void Awake()
     {
-        // 최하위 자식에 있는 모든 파티클 가져옴
         _particles = GetComponentsInChildren<ParticleSystem>(true);
     }
 
     private void OnEnable()
     {
-        if (_particles != null)
+        _despawnCts?.Cancel();
+        _despawnCts?.Dispose();
+        _despawnCts = new CancellationTokenSource();
+
+        foreach (var ps in _particles)
         {
-            foreach (var ps in _particles)
+            if (ps == null) continue;
+
+            if (!ps.gameObject.activeSelf)
             {
-                ps.Clear(true); //  이전에 남아있던 잔상과 내부 타이머를 완전히 리셋
-                ps.Play(true);  //  파티클 다시 재생 시작
+                ps.gameObject.SetActive(true);
             }
+
+            ps.Clear(true);
+            ps.Play(true);
         }
+
+        AutoDespawnAsync(_despawnCts.Token).Forget();
     }
 
-    //기존
-    //private void OnEnable()
-    //{
-    //    if (_particle == null)
-    //        _particle = GetComponent<ParticleSystem>();
+    private void OnDisable()
+    {
+        _despawnCts?.Cancel();
+        _despawnCts?.Dispose();
+        _despawnCts = null;
+    }
+    private async UniTaskVoid AutoDespawnAsync(CancellationToken token)
+    {
+        try
+        {
+            await UniTask.WaitUntil(AllParticlesFinished, cancellationToken: token);
 
-    //    if (_particle != null)
-    //    {
-    //        _particle.Clear();
-    //        _particle.Play();
+            if (PoolManager.Instance != null)
+                PoolManager.Instance.DespawnToPool(gameObject);
+            else
+                Destroy(gameObject);
+        }
+        catch (System.OperationCanceledException)
+        { }
+    }
 
-    //        // 파티클 재생 완료 후 자동 반납
-    //        AutoDespawnAsync().Forget();
-    //    }
-    //}
-
-    //private async UniTaskVoid AutoDespawnAsync()
-    //{
-    //    // 파티클이 멈출 때까지 대기
-    //    await UniTask.WaitUntil(() => _particle != null && !_particle.isPlaying, cancellationToken: this.GetCancellationTokenOnDestroy());
-
-    //    // 이펙트 자가 반납
-    //    if (PoolManager.Instance != null)
-    //    {
-    //        PoolManager.Instance.DespawnToPool(gameObject);
-    //    }
-    //    else
-    //    {
-    //        Destroy(gameObject);
-    //    }
-    //}
+    private bool AllParticlesFinished()
+    {
+        foreach (var ps in _particles)
+        {
+            if (ps != null && ps.IsAlive(true))
+                return false;
+        }
+        return true;
+    }
 }
